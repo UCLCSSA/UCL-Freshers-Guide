cce9d80ad95fe70e0bd4b7b002e3a8c2e4f7d5e9
uclcssa-info <uclcssa-info@users.noreply.github.com>
2020-08-20 08:18:05 +0000

    Deployed 5c90cf5 with MkDocs version: 1.1.2

---
+++ js/baidu.js
@@ -0,0 +1,7 @@
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?faae150447829431909805a8477a64f4";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
